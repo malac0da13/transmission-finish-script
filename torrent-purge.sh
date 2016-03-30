2951c29c78c4aa053573523b66bfddbfc2ee02f6
Michael Benson II <malac0da13@gmail.com>
2016-03-30 12:09:03 -0400

    This one works for sure now...

--- torrent-purge.sh
+++ torrent-purge.sh
@@ -2,10 +2,12 @@
 
 SERVER="9025 --auth john-peters:malac0da"
 
-NUMTORRENTS=`transmission-remote $SERVER --list | wc -l`
-let NUMTORRENTS-=2
+# Create list of torrent IDs
+TORRENTLIST=`transmission-remote $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
 
-for ((i=1; i<=NUMTORRENTS; i++)); do
+#check status of each torrent in list for '100%'
+for i in $TORRENTLIST; do
+    echo $i
 	if `transmission-remote $SERVER --torrent $i --info | grep -q "Percent Done: 100%"`; then
 		# write finished file title into text file
 		transmission-remote $SERVER --torrent $i --info | sed -e '3q;d' | cut -d' ' -f4- >> ~/finished-torrents.txt
